95bdefc21b887ae574aa25187542002912f2e207
Kieran Quinn <kieran.quinn@hotmail.co.uk>
2013-03-24 13:13:00 +0000

    updated config.h

--- config.h
+++ config.h
@@ -9,22 +9,22 @@
 
 static const unsigned int systrayspacing	= 2;	    // systray spacing
 static const Bool showsystray				= True;	    // false means no systray
-static const unsigned int gappx 			= 8;		// gaps between windows
-static const unsigned int borderpx  		= 1;        // border pixel of windows
-static const unsigned int snap      		= 32;       // snap pixel
-static const Bool showbar           		= True;     // false means no bar
-static const Bool topbar            		= True;     // false means bottom bar
-static const float mfact      				= 0.55; 	// factor of master area size [0.05..0.95]
-static const int nmaster      				= 1;   		// number of clients in master area
-static const Bool resizehints 				= False; 	// true means respect size hints in tiled resizals
+static const unsigned int gappx				= 8;		// gaps between windows
+static const unsigned int borderpx			= 1;        // border pixel of windows
+static const unsigned int snap				= 32;       // snap pixel
+static const Bool showbar					= True;     // false means no bar
+static const Bool topbar					= True;     // false means bottom bar
+static const float mfact					= 0.55; 	// factor of master area size [0.05..0.95]
+static const int nmaster					= 1;   		// number of clients in master area
+static const Bool resizehints				= False; 	// true means respect size hints in tiled resizals
 
 static const char font[] = "-*-stlarch-*-*-*-*-14-*-*-*-*-*-*-*"",""-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
 
 static const char colors[NUMCOLORS][ColLast][13] = {
     /* border    fg       	bg */
-	{ "#EEEEEE", "#666666", "#222222" },            	// 01 - regular
-	{ "#222222", "#FFFFFF", "#222222" },            	// 02 - selected
-	{ "#222222", "#FF0000", "#222222" },            	// 03 - urgent
+	{ "#EEEEEE", "#666666", "#222222" },				// 01 - regular
+	{ "#222222", "#FFFFFF", "#222222" },				// 02 - selected
+	{ "#222222", "#FF0000", "#222222" },				// 03 - urgent
 	{ "#222222", "#333333", "#222222" }, 				// 04 - occupied
 	{ "#222222", "#A82222", "#222222" }, 				// 05 - red
 	{ "#222222", "#1F7B94", "#222222" }, 				// 06 - blue
@@ -38,7 +38,7 @@ static const char colors[NUMCOLORS][ColLast][13] = {
 };
 
 static const Layout layouts[] = {
-	/* symbol			gaps		arrange */
+	/*	symbol			gaps		arrange */
 	{ "    \uE002",		True,	    tile	},
 	{ "    \uE003",   	True,	    bstack	},
 	{ "    \uE000",  	False,	    monocle	},
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 static const Tag tags[] = {
-	/* name			layout     		mfact       nmaster */
+	/* name			layout			mfact		nmaster */
 	{ "     web",	&layouts[0],  	-1,		    -1 },
 	{ "     chat",	&layouts[0],   	-1,		    -1 },
 	{ "     term",	&layouts[3],  	-1,		    -1 },
@@ -57,12 +57,12 @@ static const Tag tags[] = {
 
 static const Rule rules[] = {
     /* class         	instance   	title 	    tags mask    	isfloating		iscentred   	monitor */
-	{ "Chromium",  		NULL,       NULL,       1 << 0,    		False,   		False,			-1 },
-	{ "Skype",   		NULL,       NULL,       1 << 1,       	False,       	False,			-1 },
-	{ "URxvt",  		NULL,       NULL,       1 << 2,       	True,       	True,			-1 },
+	{ "Chromium",		NULL,       NULL,       1 << 0,    		False,   		False,			-1 },
+	{ "Skype",			NULL,       NULL,       1 << 1,       	False,       	False,			-1 },
+	{ "URxvt",			NULL,       NULL,       1 << 2,       	True,       	True,			-1 },
 	{ "Geany",			NULL,       NULL,       1 << 3,       	False,       	False,			-1 },
-	{ "Vlc",   			NULL,       NULL,       1 << 4,       	False,       	False,			-1 },
-	{ "Pcmanfm",   		NULL,       NULL,       1 << 5,       	False,       	False,			-1 },
+	{ "Vlc",			NULL,       NULL,       1 << 4,       	False,       	False,			-1 },
+	{ "Pcmanfm",		NULL,       NULL,       1 << 5,       	False,       	False,			-1 },
 };
 
 static const char *menu[] = { "dmenu_run", "-i", "-fn", font, "-nb", colors[0][ColBG], "-nf", colors[0][ColFG], "-sb", colors[1][ColBG], "-sf", colors[1][ColFG], NULL };
@@ -72,7 +72,7 @@ static const char *term[] = { "urxvtc", NULL, "URxvt" };
 static const char *edit[] = { "geany", NULL, "Geany" };
 static const char *mdia[] = { "vlc", NULL, "Vlc" };
 static const char *file[] = { "pcmanfm", NULL, "Pcmanfm" };
-static const char *prts[] = { "scrot", NULL, "~/Pictures" };
+static const char *prts[] = { "scrot", NULL };
 
 static Key keys[] = {
 	{ MONKEY,       	XK_p, 		spawn,          {.v = menu } },
@@ -90,7 +90,7 @@ static Key keys[] = {
 	{ MODKEY,          	XK_Tab,   	view,           {0} },
 	{ MODKEY|ShiftMask,	XK_f,   	togglefloating, {0} },
 	{ MODKEY,      		XK_space,	setlayout,      {0} },
-	{ MODKEY,           XK_t,    	setlayout,      {.v = &layouts[0] } },
+	{ MODKEY,			XK_t,    	setlayout,      {.v = &layouts[0] } },
 	{ MODKEY,          	XK_b,    	setlayout,      {.v = &layouts[1] } },
 	{ MODKEY,         	XK_m,  		setlayout,      {.v = &layouts[2] } },
 	{ MODKEY,          	XK_f,   	setlayout,      {.v = &layouts[3] } },
