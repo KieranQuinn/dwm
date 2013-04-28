e4ea17703306f3d7b45bb7a37aebf04a7f1fb2e0
Kieran Quinn <kieran.quinn@hotmail.co.uk>
2013-04-28 22:52:22 +0100

    update

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const char font[] = "-*-ohsnap.icons-medium-r-*-*-14-*-*-*-*-*-*-*";
 static const char colors[NUMCOLORS][ColLast][13] = {
     /* border    fg         bg */
     { "#2D2D2D", "#FFFFFF", "#2D2D2D" },        /* 01 - regular */
-    { "#1F7B94", "#FFFFFF", "#2D2D2D" },        /* 02 - selected */
+    { "#D64937", "#FFFFFF", "#2D2D2D" },        /* 02 - selected */
     { "#2D2D2D", "#FF0000", "#2D2D2D" },        /* 03 - urgent */
     { "#2D2D2D", "#666666", "#2D2D2D" },        /* 04 - occupied */
     { "#2D2D2D", "#A82222", "#2D2D2D" },        /* 05 - red */
